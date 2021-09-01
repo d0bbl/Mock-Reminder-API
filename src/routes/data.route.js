4e50c3e211c6aa64eca4832cff43851beda5cae6
dobbleXg <kennybajomo@gmail.com>
2021-09-01 22:56:42 +0100

    added search handler function

--- src/routes/data.route.js
+++ src/routes/data.route.js
@@ -4,13 +4,14 @@ const dataController = require("../controllers/data.controller");
 const router = express.Router();
 
 router.get('/data/read/:plugin_id/:collection_name/:organization_id', dataController.find);
+
 router.post('/data/read/:plugin_id/:collection_name/:organization_id', dataController.search);
 
 router.post("/data/write", dataController.create);
 
 router.put("/data/write", dataController.update);
 
-// router.delete("/data/write", dataController.delete);
+router.delete("/data/write", dataController.delete);
 
 
 module.exports = router;
